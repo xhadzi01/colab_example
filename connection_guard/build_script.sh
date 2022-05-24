6cad5c0063a1b80449a2babeb8a814d90ceaca29
xhadzi01 <killjeaden52@gmail.com>
2022-05-24 22:19:11 +0200

    comments

--- connection_guard/build_script.sh
+++ connection_guard/build_script.sh
@@ -4,6 +4,7 @@ echo Building image: connection_guard
 
 
 # we will be using kind of a hack here, sice we cant assign MAC address to docker build,
-# but we can assign MAC to container and ther inherit from it
+# but we can assign MAC to container and then inherit from it. This is done to conceal our MAC address from connected to services.
+# TODO: Create automated way to generate MACs.
 docker run --rm --name mac_workaround --mac-address="12:34:de:b0:6b:61" -d alpine tail -f /dev/null
 docker build -t connection_guard --network container:mac_workaround --no-cache .
